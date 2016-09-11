6a80f2fbdadb33448151dcb126d3db036bf452de
nishlfernando11 <nlfernando11@gmail.com>
2016-09-11 17:53:44 +0530

    commit code base

---
+++ FilterWithMVEL/src/main/java/com/filter/App.java
@@ -0,0 +1,13 @@
+package main.java.com.filter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
